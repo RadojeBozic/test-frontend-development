b6ec12d77ecde96c369011449cfabb707e4ebd21
Radoje Bozic <radojebozic1966@gmail.com>
2024-05-10 19:01:11 +0200

    Dodavanje_prvo

---
+++ js/app.js
@@ -0,0 +1,6 @@
+document.querySelector("#registracija").addEventListener("click", () => {
+    document.querySelector(".custom-modal").style.display = "block";
+});
+document.querySelector("#closeModal").addEventListener("click", () => {
+    document.querySelector(".custom-modal").style.display = "none";
+});
